59ddeeac2ce96e2f11f140cb63daa84c76e38005
Arjun-Regmi-Chhetri <arjunxetri100@gmail.com>
2024-02-05 16:44:54 +0545

    Python Upto List

---
+++ 1. Syntax/example.py
@@ -0,0 +1,36 @@
+
+
+
+# syntax one------------------------------------
+
+print("Hello World")
+
+
+#syntax two--------------------------------------
+
+if 10 > 5:
+    print("10 is greater")
+
+
+
+#syntax three------------------------------------
+    
+    
+    """
+      this syntax here throw error as there is no proper indentation
+    """
+    
+if 10 > 5:
+    print("10 is greater")
+    else:
+        print("5 is greater") 
+
+
+
+
+#corrected syntax three--------------------------
+    
+if 10 > 5:
+    print("10 is greater")
+else:
+     print("5 is greater")   
